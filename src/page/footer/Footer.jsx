4f9bbc30cfc87361a13d875dd698271d067565bb
Blessing <31315580+blessingsunday1995@users.noreply.github.com>
2023-02-04 13:19:07 +0100

    .

--- src/page/footer/Footer.jsx
+++ src/page/footer/Footer.jsx
@@ -1,5 +1,6 @@
 import {FaFacebookF} from 'react-icons/fa'
 import { BsInstagram } from 'react-icons/bs'
+import {Link, Route,Routes} from 'react-router-dom'
 import {BsTwitter} from 'react-icons/bs'
 import './footer.css'
 
@@ -9,20 +10,21 @@ const Footer = () => {
     <div className='footer-container'>
         <div className="cx56text">
           <h1>CX-365</h1>
-          <a href="#"> <FaFacebookF/></a>
-         <a href="#"><BsTwitter /></a>
-          <a href="#"> <BsInstagram /></a>
+          <a href="https://www.facebook.com/profile.php?id=100089633372799&mibextid=ZbWKwL
+"> <FaFacebookF/></a>
+         <a href="https://twitter.com/CX365_CRM" ><BsTwitter /></a>
+          <a href="https://www.instagram.com/cx365_crm/"> <BsInstagram /></a>
          
         </div>
 
           <div className="quck-link">
             <h1>Quick Link</h1>
              <ul>
-            <li><a href="">Home</a> </li>
+            <li><Link to='/'>Home</Link></li>
             <li><a href="">How it works</a> </li>
-            <li> <a href="">About Us</a></li>
-            <li><a href="">Get Signed Up </a></li>
-            <li> <a href="">Login</a> </li>
+            <li>  <Link to='/about'>About Us </Link></li>
+            <li><Link to='/sign_up'>Get Signed Up </Link></li>
+            <li> <Link to='/'>Login</Link> </li>
           </ul>
           </div>
 
