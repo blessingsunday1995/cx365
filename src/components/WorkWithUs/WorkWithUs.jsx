13ceb503f5ccae88f296556db341c6b712e597af
cem <blessing.sunday38@yahoo.com>
2023-02-16 18:01:32 +0100

    .

---
+++ src/components/WorkWithUs/WorkWithUs.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function WorkWithUs() {
+  return (
+    <div className='WorkWithUs'>
+        <h2>Why Work with Us?</h2>
+            <div className="crm"></div>
+    
+    </div>
+  )
+}
+
+export default WorkWithUs
--- src/page/LandingPage/LandingPage.jsx
+++ src/page/LandingPage/LandingPage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import FristPage from '../../components/fristPage/FristPage'
 import Navbar from '../../components/Navbar/Navbar'
+import WorkWithUs from '../../components/WorkWithUs/WorkWithUs'
 
 function LandingPage() {
   return (
     <div>
       <Navbar/>
       <FristPage/>
+      <WorkWithUs/>
     </div>
   )
 }
